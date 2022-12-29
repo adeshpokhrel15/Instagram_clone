f0819d7699a8649a3cb9f7f7a4c2017746ec864f
adeshpokhrel15 <adeshpokhrel15@gmail.com>
2022-12-29 19:06:52 +0545

    Login UI

--- lib/features/home/login_screen.dart
+++ lib/features/home/login_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/features/users/widgets/text_field_input.dart';
 
+import '../../constant/app_constant/app_constant.dart';
+
 class LoginScreen extends StatelessWidget {
   final mailController = TextEditingController();
   final passwordController = TextEditingController();
@@ -46,7 +48,52 @@ class LoginScreen extends StatelessWidget {
                 textInputType: TextInputType.visiblePassword,
                 isPass: true,
               ),
+              SizedBox(
+                height: 10,
+              ),
               //button
+              InkWell(
+                child: Container(
+                  child: Text("Log In"),
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.symmetric(vertical: 20),
+                  decoration: ShapeDecoration(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    color: AppConstant.blueColor,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Flexible(
+                child: Container(),
+                flex: 2,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    child: Text("Don't have an account?"),
+                    padding: EdgeInsets.symmetric(vertical: 8),
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      child: Text(
+                        "Sign Up",
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                      padding: EdgeInsets.symmetric(vertical: 8),
+                    ),
+                  ),
+                ],
+              )
             ],
           ),
         )));
